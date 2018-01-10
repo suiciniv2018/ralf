1ebeea4897a3fd3e1e467872c852478fcbd76d7c
T7239935 <T7239935@nsaodt5nlyfm2.netservicos.corp>
2018-01-10 11:15:27 -0200

    conexão com o banco de dados( pesquisando) pagina 2 já está gerando mas mas não está consultando

---
+++ public_html/bootstrap/js/button.js
@@ -0,0 +1,14 @@
+<html>
+ <head>
+  <title>.:: Página Principal ::.</title>
+  <link rel="stylesheet" type="text/css" href="estilo.css">
+ </head>
+ <body>
+  <script language="javascript"></script>
+
+function consulta(){
+ 
+ }
+</head>
+
+</html>
